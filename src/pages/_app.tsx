af5f33f4ecbe85572421d5391d7fd71e40c7e8e4
Luis Gabriel <janco7249@gmail.com>
2021-12-06 23:55:28 -0400

    refactor: add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className="bg-blue-200 dark:bg-indigo-900 min-h-screen grid place-content-center">
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
-import type { NextPage } from 'next'
 import React from 'react'
 
-const Home: NextPage = () => {
-  return (
-    <h1>hello world</h1>
-  )
+interface Props {
+  books: Book[];
 }
 
+const Home = ({ books }: Props) => {
+  console.log(books)
+  // const [darkMode, setDarkMode] = useState(false)
+  // const handleClick = () => {
+  //   document.documentElement.classList.toggle('dark')
+  //   setDarkMode(!darkMode)
+  // }
+  // const text = !darkMode ? 'dark mode' : 'light mode'
+  return books.length === 0 ? <h1>No books</h1> : <h1>Books</h1>
+}
 export default Home
+
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/books')
+  const books = await res.json()
+  return {
+    props: {
+      books: books,
+    },
+  }
+}
