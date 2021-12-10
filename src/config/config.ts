41a91fb5c061fef169ff97b0191eb1eaeb8884a7
Luis Gabriel <janco7249@gmail.com>
2021-12-09 22:31:49 -0400

    add dependencies

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,10 @@
 export default {
   MONGO_DATABASE: process.env.MONGODB_URI || '',
   MONGO_DATABASE_TEST: process.env.MONGODB_URI_TEST || '',
+  jwtSecret: process.env.JWT_SECRET || 'tokentest',
+  connectionURI: process.env.CONNECTION_URI || '',
+  clientGoogle: process.env.GOOGLE_CLIENT || '',
+  clientGoogleSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+  clientGitHub: process.env.GITHUB_CLIENT || '',
+  clientGitHubSecret: process.env.GITHUB_CLIENT_SECRET || '',
 }
