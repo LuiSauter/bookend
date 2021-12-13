2ea490186378f58b91c51a7451bc4744d86d121b
Luis Gabriel <janco7249@gmail.com>
2021-12-13 10:20:09 -0400

    update settings to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,39 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        small: '.6rem',
+      },
+      colors: {
+        primary: '#06141D',
+        secondary: '#1B2730',
+        secondaryHover: '#1a252d',
+        secondaryLigth: '#28343E',
+        thirdPurple: '#5046bb',
+        thirdBlue: '#0099ff',
+        fourth: '#ffc540',
+        textWhite: '#F5FDFF',
+        textGray: '#4D5B66',
+      },
+      gridTemplateColumns: {
+        mobile: '62px 1fr',
+        laptop: '1fr 2fr',
+        desktop: '1fr 3fr 1fr',
+      },
+      maxWidth: {
+        aside: '280px',
+        maxAside: '325px',
+        post: '596px',
+        maxPost: '615px',
+      },
+      minWidth: {
+        minAside: '230px',
+      },
+      minHeight: {
+        nav: '48px',
+      },
+    },
   },
   variants: {
     extend: {
