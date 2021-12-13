973819269607954d14ebdc563b22c7ca6d5a87b8
Luis Gabriel <janco7249@gmail.com>
2021-12-13 18:57:54 -0400

    feat: add icons

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+export const home = (<svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6 md:h-5 md:w-5 md:mx-2"
+  fill="none"
+  viewBox="0 0 22 24"
+  stroke="currentColor"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+  />
+</svg>)
+
+export const homeCurrent = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 md:h-5 md:w-5 md:mx-1"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    stroke="currentColor"
+  >
+    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+  </svg>
+)
+
+export const book = (<svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6 md:h-5 md:w-5 md:mx-2"
+  fill="none"
+  viewBox="0 0 22 24"
+  stroke="currentColor"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+  />
+</svg>)
+export const bookCurrent = (<svg
+  xmlns="http://www.w3.org/2000/svg"
+  // className="h-5 w-5"
+  className="h-6 w-6 md:h-5 md:w-5 md:mx-2"
+  viewBox="0 0 20 20"
+  stroke="currentColor"
+  fill="currentColor"
+>
+  <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
+</svg>)
+
+export const notification = (<svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6 md:h-5 md:w-5 md:mx-2"
+  fill="none"
+  viewBox="0 0 22 24"
+  stroke="currentColor"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+  />
+</svg>)
+export const notificationCurrent = (<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-5 md:w-5 md:mx-2" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor">
+  <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+</svg>)
+
+export const message = (<svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6 md:h-5 md:w-5 md:mx-2"
+  fill="none"
+  viewBox="0 0 22 24"
+  stroke="currentColor"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+  />
+</svg>)
+export const messageCurrent = (<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-5 md:w-5 md:mx-2" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor">
+  <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
+</svg>)
+
+export const search = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </svg>
+)
