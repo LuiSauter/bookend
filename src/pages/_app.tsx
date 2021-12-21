adefab16654403faaaa9ac62350a756789b5b03f
Luis Gabriel <janco7249@gmail.com>
2021-12-20 23:12:20 -0400

    update base url vercel domain

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: 'https://bookendd.vercel.app/api/graphql',
   }),
 })
 
