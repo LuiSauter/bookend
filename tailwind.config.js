f9058e93265a8c27b50379039682ab34516d7f48
Luis Gabriel Janco <88288135+LuiSauter@users.noreply.github.com>
2021-12-22 00:16:36 -0400

    changes tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,}',
+    './src/assets/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
