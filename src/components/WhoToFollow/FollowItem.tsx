a1525c3d311531e9ac44cc1ee852869f8116eb0f
Luis Gabriel <janco7249@gmail.com>
2021-12-23 19:32:34 -0400

    add component Who to follow

---
+++ src/components/WhoToFollow/FollowItem.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { FIND_PROFILE, FIND_USER } from 'src/users/graphql-queries'
+import { useMutation, useQuery } from '@apollo/client'
+import { FOLLOW_USER, UNFOLLOW_USER } from 'src/users/graphql-mutations'
+import { useProfileId } from 'src/hooks/useProfileId'
+import { checkVeriFied } from 'src/assets/icons'
+
+interface Props {
+  email: string;
+  name: string;
+  photo: string;
+  user: string;
+  username: string;
+  verified: boolean;
+}
+
+const FollowItem = ({
+  email,
+  name,
+  photo,
+  user,
+  username,
+  verified,
+}: Props) => {
+  const { data: session } = useSession()
+  const router = useRouter()
+  const { profile } = useProfileId()
+  const { data: dataUser } = useQuery(FIND_USER, {
+    variables: { email: session?.user?.email },
+  })
+  const [getFollow] = useMutation(FOLLOW_USER, {
+    refetchQueries: [
+      { query: FIND_USER, variables: { email: session?.user?.email } },
+      {
+        query: FIND_PROFILE,
+        variables: { username: router.query.username || profile },
+      },
+    ],
+  })
+
+  const [getUnFollow] = useMutation(UNFOLLOW_USER, {
+    refetchQueries: [
+      { query: FIND_USER, variables: { email: session?.user?.email } },
+      {
+        query: FIND_PROFILE,
+        variables: { username: router.query.username || profile },
+      },
+    ],
+  })
+  const handleClickLi = (data: string) => {
+    router.push(`/${data}`)
+  }
+
+  const handleClickButtonFollow = (data: string) => {
+    getFollow({ variables: { user: data, email: session?.user?.email } })
+  }
+
+  const handleClickButtonUnFollow = (data: string) => {
+    getUnFollow({ variables: { user: data, email: session?.user?.email } })
+  }
+
+  const isMatch = dataUser?.findUser?.following.some(
+    (userId: string) => userId === user
+  )
+  console.log(verified)
+  return (
+    <>
+      {session?.user?.email !== email && (
+        <li
+          className="flex justify-center items-center relative mx-3 p-3 rounded-xl hover:bg-secondaryLigth transition-colors cursor-pointer"
+          onClick={() => handleClickLi(username)}
+        >
+          <img src={photo} alt={name} className="w-12 h-12 rounded-full mr-3" />
+          <div className="flex flex-row w-full justify-between items-center relative">
+            <Link href={`/${username}`}>
+              <a className="flex flex-col overflow-hidden w-full">
+                <h3 className="hover:underline font-semibold flex items-center">
+                  {name}{' '}
+                  <span title="Verified account">
+                    {verified && checkVeriFied}
+                  </span>
+                </h3>
+                <span className="text-textGray text-sm">@{username}</span>
+              </a>
+            </Link>
+            {!isMatch ? (
+              <button
+                onClick={(event) => {
+                  event.stopPropagation()
+                  handleClickButtonFollow(user)
+                }}
+                className="bg-blue-400 hover:bg-thirdBlue text-textWhite transition-colors font-medium text-sm rounded-2xl px-3 py-1 relative"
+              >
+                Follow
+              </button>
+            ) : (
+              <button
+                onClick={(event) => {
+                  event.stopPropagation()
+                  handleClickButtonUnFollow(user)
+                }}
+                className="bg-transparent border border-textGray  hover:bg-red-500 hover:border-red-500 transition-colors font-medium text-sm rounded-2xl px-3 py-1 relative"
+              >
+                Unfollow
+              </button>
+            )}
+          </div>
+        </li>
+      )}
+    </>
+  )
+}
+
+export default FollowItem
--- src/components/WhoToFollow/WhoToFollow.tsx
+++ src/components/WhoToFollow/WhoToFollow.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import { useQuery } from '@apollo/client'
+import React, { useEffect, useState } from 'react'
+import { ALL_USERS } from 'src/users/graphql-queries'
+import FollowItem from './FollowItem'
+interface IUser {
+  email: string;
+  name: string;
+  photo: string;
+  username: string;
+  user: string;
+  verified: boolean;
+}
 
 const WhoToFollow = () => {
+  const { data, loading } = useQuery(ALL_USERS)
+  const [allUser, setAllUsers] = useState<IUser[]>([] as IUser[])
+
+  useEffect(() => {
+    let cleanup = true
+    if (cleanup) {
+      if (data?.allUsers) {
+        setAllUsers(data?.allUsers)
+      }
+    }
+    return () => {
+      cleanup = false
+    }
+  }, [data?.allUsers])
+
   return (
-    <article className="w-full bg-secondary rounded-xl px-4">
-      <h2>Who to follow</h2>
-      <div>xd</div>
+    <article className="w-full bg-secondary rounded-xl py-4 relative">
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <ul className="flex w-full flex-col relative overflow-hidden">
+          {allUser.length !== 0 &&
+            allUser.map((user: IUser) => (
+              <FollowItem
+                key={user.user}
+                name={user.name}
+                email={user.email}
+                photo={user.photo}
+                user={user.user}
+                username={user.username}
+                verified={user.verified}
+              />
+            ))}
+        </ul>
+      )}
     </article>
   )
 }
