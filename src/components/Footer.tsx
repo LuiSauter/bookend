c57d10387631b32b08896dba66823015c9172d1e
Luis Gabriel <janco7249@gmail.com>
2021-12-23 19:32:54 -0400

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="flex flex-row flex-wrap text-sm px-4 gap-4">
+        <Link href="/about">
+          <a className="text-textGray hover:underline">About</a>
+        </Link>
+        <Link href="https://github.com/LuiSauter/bookend">
+          <a className="text-textGray hover:underline" target="_blank">
+            Github
+          </a>
+        </Link>
+        <span className="text-textGray">
+          © 2021 Bookend,{' '}
+          <Link href="https://sauterdev.vercel.app/">
+            <a className="hover:underline" target="_blank">
+              Luis G. Janco
+            </a>
+          </Link>
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default Footer
