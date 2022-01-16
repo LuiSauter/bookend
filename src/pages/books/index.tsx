cd290a4b096a12f6f973b090a26650ab4c1d579f
Luis Gabriel <janco7249@gmail.com>
2022-01-16 17:27:58 -0400

    add all books in new page

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import ClientOnly from 'src/components/ClientOnly'
+import AllPosts from 'src/components/Post/AllPosts'
+import Head from 'next/head'
 
 const Books = () => {
   return (
-    <section className='text-red-500 grid place-content-center place-items-center'>
-      <h1>Welcome to store books not available</h1>
-    </section>
+    <>
+      <Head>
+        <title>Bookend | Books</title>
+      </Head>
+      <section className='text-red-500 grid place-content-center place-items-center'>
+        <h1>Welcome to store books not available</h1>
+      </section>
+      <ClientOnly>
+        <AllPosts />
+      </ClientOnly>
+    </>
   )
 }
 
