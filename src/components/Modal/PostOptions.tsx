a5076b34d27dd120cdbbdbe2dc6592a801e4996e
Luis Gabriel <janco7249@gmail.com>
2022-01-27 23:17:56 -0400

    add options to edit post

--- src/components/Modal/PostOptions.tsx
+++ src/components/Modal/PostOptions.tsx
@@ -1,26 +1,38 @@
+import { useRouter } from 'next/router'
 import React from 'react'
-import { useToggleUser } from 'src/hooks/useToggleUser'
 import Button from '../Button/Button'
 
+interface Props {
+  id: string | undefined
+  toggleOptions?: () => void
+}
 
-const PostOptions = () => {
-  const { handleOptions } = useToggleUser()
+
+const PostOptions = ({ id, toggleOptions }: Props) => {
+  const router = useRouter()
+
   return (
-    <div className='fixed inset-0 w-full h-full grid place-content-center place-items-center z-[50]'>
-      <div className='bg-secondary rounded-xl p-4 flex flex-col w-[60vw] h-min z-[60] transition-all sm:w-80'>
-        <Button onClick={handleOptions} color={'hover:bg-secondaryLigth'}>
+    <div className='fixed inset-0 w-full h-full grid place-content-center place-items-center z-[80]'>
+      <div className='bg-secondary rounded-xl p-4 flex flex-col w-[60vw] h-min z-[90] transition-all sm:w-80'>
+        <Button
+          onClick={() => console.log('button not available')}
+          color={'hover:bg-secondaryLigth'}
+        >
           Share
         </Button>
-        <Button onClick={handleOptions} color={'hover:bg-secondaryLigth'}>
+        <Button
+          onClick={() => router.push(`/books/new/${id}`)}
+          color={'hover:bg-secondaryLigth'}
+        >
           Edit
         </Button>
-        <Button onClick={handleOptions} color={'bg-red-400 hover:bg-red-500'}>
+        <Button onClick={toggleOptions} color={'bg-red-400 hover:bg-red-500'}>
           Cancel
         </Button>
       </div>
       <div
-        onClick={handleOptions}
-        className='fixed top-0 left-0 right-0 bg-black/40 transition-all h-screen w-full z-[50]'
+        onClick={toggleOptions}
+        className='fixed top-0 left-0 right-0 bg-black/40 transition-all h-screen w-full z-[80]'
       />
     </div>
   )
