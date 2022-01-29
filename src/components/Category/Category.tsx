c5c3f31146d8fcbb27fff1795753a849b58595ce
Luis Gabriel <janco7249@gmail.com>
2022-01-29 11:51:58 -0400

    [style] update layout, change in max width

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -3,11 +3,11 @@ import { categorys } from 'src/assets/data/category'
 
 const Category = () => {
   return (
-    <ul className='relative w-full py-2'>
+    <ul className='relative w-full p-2'>
       {categorys.map((category: string, index: number) => (
         <li
           key={index}
-          className='px-4 py-1 flex flex-row flex-wrap justify-between hover:bg-secondaryLigth relative mx-2 rounded-lg'
+          className='px-4 w-full py-1 flex flex-row flex-wrap justify-between hover:bg-secondaryLigth relative  rounded-lg'
         >
           <div>
             <span className='text-textGray mr-2'>#</span>
