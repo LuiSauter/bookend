bc631a3b21584c4d76cd188ee0f9604e64ca455d
Luis Gabriel <janco7249@gmail.com>
2022-02-23 17:44:00 -0400

    add loading page in app

--- src/layouts/LoadingPage.tsx
+++ src/layouts/LoadingPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-
 export const LoadingPage = () => {
   return (
     <>
-      <div className='bg-primary text-textWhite grid place-content-center place-items-center min-h-screen w-screen'>
-        loading...
+      <div className='dark:bg-primary bg-white text-textWhite grid place-content-center place-items-center min-h-screen w-screen'>
+        <img
+          src='/images/bookend-logo.png'
+          alt='Bookend libros gratis'
+          className='h-28 flex flex-shrink'
+        />
       </div>
     </>
   )
