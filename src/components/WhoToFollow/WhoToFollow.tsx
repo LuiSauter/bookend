3d5e8459246f8f60e5ad019a6b80e832a0787d8e
Luis Gabriel <janco7249@gmail.com>
2022-04-10 15:51:09 -0400

    show only if is verified

--- src/components/WhoToFollow/WhoToFollow.tsx
+++ src/components/WhoToFollow/WhoToFollow.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { LoadingIcon } from 'src/assets/icons/LoadingIcon'
 import { useTranslate } from 'src/hooks/useTranslate'
@@ -14,6 +15,7 @@ interface IUser {
 }
 
 const WhoToFollow = () => {
+  const router = useRouter()
   const { data, loading } = useQuery(ALL_USERS, {
     ssr: true,
   })
@@ -35,24 +37,32 @@ const WhoToFollow = () => {
   return (
     <>
       {loading ? (
-        <div className='py-2'>
+        <div className='pt-2 pb-4'>
           <LoadingIcon />
         </div>
       ) : (
         <ul className='flex w-full flex-col overflow-x-auto relative overflow-hidden'>
           {allUser.length !== 0 &&
-            allUser.map((user: IUser) => (
-              <FollowItem
-                key={user.user}
-                name={user.name}
-                email={user.email}
-                photo={user.photo}
-                user={user.user}
-                username={user.username}
-                verified={user.verified}
-              />
-            ))}
-          <button className='text-center p-2 hover:text-textGray'>{translate.home.searchBook.showMore}</button>
+            allUser.map(
+              (user: IUser) =>
+                user.verified && (
+                  <FollowItem
+                    key={user.user}
+                    name={user.name}
+                    email={user.email}
+                    photo={user.photo}
+                    user={user.user}
+                    username={user.username}
+                    verified={user.verified}
+                  />
+                )
+            )}
+          <button
+            onClick={() => router.push('/search/users')}
+            className='text-center p-2 dark:text-slate-400 text-slate-700 dark:hover:text-slate-400/50 hover:text-thirdBlue transition-colors'
+          >
+            {translate.home.searchBook.showMore}
+          </button>
         </ul>
       )}
     </>
