396d56bd293c6b9e135460e6a6b82cac92969162
Luis Gabriel <janco7249@gmail.com>
2022-05-13 22:40:10 -0400

    feat(Root): Add `getStaticPaths` and `getStaticProps` to ssr

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,34 +1,27 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import MyProfile from 'src/components/Profile/MyProfile'
-import MyPosts from 'src/components/Profile/MyPosts'
-import ClientOnly from 'src/components/ClientOnly'
-import { useLazyQuery, useQuery } from '@apollo/client'
 import { FIND_PROFILE, GET_DOMINANT_COLOR } from 'src/users/graphql-queries'
 import { useDominanColor } from 'src/hooks/useDominantColor'
+import { GraphqlApolloCLient } from 'src/data/ApolloClient'
+import MyProfile from 'src/components/Profile/MyProfile'
+import { useRouter } from 'next/router'
+import ClientOnly from 'src/components/ClientOnly'
+import MyPosts from 'src/components/Profile/MyPosts'
 
-const Profile = () => {
-  const router = useRouter()
-  const { username } = router.query
+interface Props {
+  user: any | null
+  dataColor?: string
+  username: string
+}
 
-  const { data } = useQuery(FIND_PROFILE, { variables: { username: username } })
+interface StaticProps {
+  params: {
+    username: string
+  }
+}
 
-  const [getDominantColor, { data: dataColor }] =
-    useLazyQuery(GET_DOMINANT_COLOR)
-
-  useEffect(() => {
-    let monted = true
-    if (monted) {
-      data?.findProfile.me.photo &&
-        getDominantColor({ variables: { image: data?.findProfile.me.photo } })
-    }
-    return () => {
-      monted = false
-    }
-  }, [data?.findProfile])
-
-  const { dominantColor } = useDominanColor(dataColor?.getColors)
+const Profile = ({ user, dataColor, username }: Props) => {
+  const { dominantColor } = useDominanColor(dataColor)
 
   return (
     <>
@@ -36,28 +29,57 @@ const Profile = () => {
         <title>Bookend | {username ? username : 'Loading...'}</title>
         <meta name='description' content={`${username}, profile in Bookend`} />
       </Head>
-      <>
-        <section
-          className='w-full h-full rounded-xl py-4 mx-auto
+      <section
+        className='w-full h-full rounded-xl py-4 mx-auto
             sm:md:min-w-[430px]'
-        >
-          <header className='m-0 w-full z-0'>
-            <div
-              style={{
-                backgroundColor: dominantColor,
-              }}
-              className='bg-backgroundImageFronPage absolute top-12 md:top-0 w-full h-32 sm:h-36 md:h-44'
-            />
-          </header>
-          <ClientOnly>
-            <MyProfile username={username} />
-          </ClientOnly>
-          <ClientOnly>
-            <MyPosts username={username} />
-          </ClientOnly>
-        </section>
-      </>
+      >
+        <header className='m-0 w-full z-0'>
+          <div
+            style={{ backgroundColor: dominantColor }}
+            className='bg-backgroundImageFronPage absolute top-12 md:top-0 w-full h-32 sm:h-36 md:h-44'
+          />
+        </header>
+        {user?.data.findProfile && (
+          <>
+            <MyProfile user={user?.data.findProfile} />
+            <ClientOnly>
+              <MyPosts user={user?.data.findProfile} />
+            </ClientOnly>
+          </>
+        )}
+      </section>
     </>
   )
 }
 export default Profile
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { username: '1' } }],
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }: StaticProps) {
+  const client = GraphqlApolloCLient()
+  const user = await client.query({
+    query: FIND_PROFILE,
+    variables: { username: params.username },
+  })
+  if (!user?.data.findProfile) {
+    return {
+      notFound: true,
+    }
+  }
+  const { data: dataColor } = await client.query({
+    query: GET_DOMINANT_COLOR,
+    variables: { image: user?.data.findProfile.me.photo },
+  })
+  return {
+    props: {
+      user,
+      dataColor: dataColor?.getColors,
+      username: params.username,
+    },
+  }
+}
