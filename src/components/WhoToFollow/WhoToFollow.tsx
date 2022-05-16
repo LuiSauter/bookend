b8816fae777d90d2e5574e419304494e614bdccf
Luis Gabriel <janco7249@gmail.com>
2022-05-15 21:13:42 -0400

    feat(Root): Add loading placeholder of type pulse to card `allUsers`

--- src/components/WhoToFollow/WhoToFollow.tsx
+++ src/components/WhoToFollow/WhoToFollow.tsx
@@ -1,18 +1,11 @@
-import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-import { LoadingIcon } from 'src/assets/icons/LoadingIcon'
+import { useQuery } from '@apollo/client'
 import { useTranslate } from 'src/hooks/useTranslate'
 import { ALL_USERS } from 'src/users/graphql-queries'
 import FollowItem from './FollowItem'
-interface IUser {
-  email: string
-  name: string
-  photo: string
-  username: string
-  user: string
-  verified: boolean
-}
+import PulseUsers from 'src/assets/icons/esqueleton/PulseUsers'
+import { IUser } from 'src/interfaces/Users'
 
 const WhoToFollow = () => {
   const router = useRouter()
@@ -37,9 +30,7 @@ const WhoToFollow = () => {
   return (
     <>
       {loading ? (
-        <div className='pt-2 pb-4'>
-          <LoadingIcon />
-        </div>
+        <PulseUsers size='h-11 w-11' n={5} font='h-2' paddingY='py-2' />
       ) : (
         <ul className='flex w-full flex-col overflow-x-auto relative overflow-hidden'>
           {allUser.length !== 0 &&
@@ -57,14 +48,14 @@ const WhoToFollow = () => {
                   />
                 )
             )}
-          <button
-            onClick={() => router.push('/search/users')}
-            className='text-center p-2 dark:text-slate-400 text-slate-700 dark:hover:text-slate-400/50 hover:text-thirdBlue transition-colors'
-          >
-            {translate.home.searchBook.showMore}
-          </button>
         </ul>
       )}
+      <button
+        onClick={() => router.push('/search/users')}
+        className='text-center w-full p-2 dark:text-slate-400 text-slate-700 dark:hover:text-slate-400/50 hover:text-thirdBlue transition-colors'
+      >
+        {translate.home.searchBook.showMore}
+      </button>
     </>
   )
 }
