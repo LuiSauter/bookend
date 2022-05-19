02fca41a732001e8d42fa588f83397c5f6831536
Luis Gabriel <janco7249@gmail.com>
2022-05-18 22:30:34 -0400

    perf(Root): Should redirect 404 when find didn't of user

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
-import { FIND_PROFILE, GET_DOMINANT_COLOR } from 'src/users/graphql-queries'
+import { ALL_USERS, FIND_PROFILE, GET_DOMINANT_COLOR } from 'src/users/graphql-queries'
 import { useDominanColor } from 'src/hooks/useDominantColor'
 import { GraphqlApolloCLient } from 'src/data/ApolloClient'
 import MyProfile from 'src/components/Profile/MyProfile'
 import ClientOnly from 'src/components/ClientOnly'
 import MyPosts from 'src/components/Profile/MyPosts'
+import { useStaticUsers } from 'src/hooks/useStaticUsers'
+import { IUser } from 'src/interfaces/Users'
 
 interface Props {
-  user: { data: { findProfile: Profile } } | null
+  user: { findProfile: Profile } | null
   dataColor?: string
   username: string
+  users: { allUsers: IUser[] }
 }
 
 interface StaticProps {params: { username: string }}
 
-const Profile = ({ username, dataColor, user }: Props) => {
+const Profile = ({ username, dataColor, user, users }: Props) => {
   const { dominantColor } = useDominanColor(dataColor)
+  const { addUsers } = useStaticUsers()
+  useEffect(() => {
+    let cleanup = true
+    if (cleanup) {
+      users && addUsers(users?.allUsers)
+    }
+    return () => {
+      cleanup = false
+    }
+  }, [users])
 
   return (
     <>
@@ -24,21 +37,18 @@ const Profile = ({ username, dataColor, user }: Props) => {
         <title>Bookend | {username ? username : 'Loading...'}</title>
         <meta name='description' content={`${username}, profile in Bookend`} />
       </Head>
-      <section
-        className='w-full h-full rounded-xl py-4 mx-auto
-            sm:md:min-w-[430px]'
-      >
+      <section className='w-full h-full rounded-xl py-4 mx-auto sm:md:min-w-[430px]'>
         <header className='m-0 w-full z-0'>
           <div
             style={{ backgroundColor: dominantColor }}
             className='bg-backgroundImageFronPage absolute top-12 md:top-0 w-full h-32 sm:h-36 md:h-44'
           />
         </header>
-        {user?.data.findProfile && (
+        {user?.findProfile && (
           <>
-            <MyProfile user={user?.data.findProfile} />
+            <MyProfile user={user?.findProfile} />
             <ClientOnly>
-              <MyPosts user={user?.data.findProfile} />
+              <MyPosts user={user?.findProfile} />
             </ClientOnly>
           </>
         )}
@@ -49,33 +59,47 @@ const Profile = ({ username, dataColor, user }: Props) => {
 export default Profile
 
 export async function getStaticPaths() {
+  const client = GraphqlApolloCLient()
+  const { data } = await client.query({ query: ALL_USERS })
+  const users: IUser[] = []
+  data?.allUsers.forEach((user:IUser) => {
+    users.push({...user, username: user.username })
+  })
+  const paths = users.map((user) => ({ params: { username: user.username } }))
   return {
-    paths: [{ params: { username: '1' } }],
-    fallback: true,
+    paths: paths,
+    fallback: false,
   }
 }
 
 export async function getStaticProps({ params }: StaticProps) {
-  const client = GraphqlApolloCLient()
-  const { username } = params
-  const user = await client.query({
-    query: FIND_PROFILE,
-    variables: { username: username },
-  })
-  if (!user?.data.findProfile) {
+  try {
+    const client = GraphqlApolloCLient()
+    const { data } = await client.query({
+      query: FIND_PROFILE,
+      variables: { username: params.username },
+    })
+    if (!data.findProfile) {
+      return {
+        notFound: true,
+      }
+    }
+    const { data: dataColor } = await client.query({
+      query: GET_DOMINANT_COLOR,
+      variables: { image: data.findProfile.me.photo },
+    })
+    const { data: allUsers } = await client.query({ query: ALL_USERS })
+    return {
+      props: {
+        user: data,
+        dataColor: dataColor?.getColors,
+        username: params.username,
+        users: allUsers
+      },
+    }
+  } catch (error) {
     return {
       notFound: true,
     }
-  }
-  const { data: dataColor } = await client.query({
-    query: GET_DOMINANT_COLOR,
-    variables: { image: user?.data.findProfile.me.photo },
-  })
-  return {
-    props: {
-      user,
-      dataColor: dataColor?.getColors,
-      username: username,
-    },
   }
 }
